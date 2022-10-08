af1ea93467aed9e248a7c6d1b0afbda6c9b2b627
tylercmac <tylercmac@gmail.com>
2022-10-07 22:09:12 -0400

    Input validation, dynamic player gen, styling

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,94 +1,35 @@
 const { useState, useEffect } = require("react")
-const { Button } = require("@mui/material")
-
+const { Button } = require('@mui/material');
 
 export default function Index() {
-  // let savedScore = typeof window !== 'undefined' ? localStorage.getItem('score1') : null
-  // let savedScore2 = typeof window !== 'undefined' ? localStorage.getItem('score2') : null
-  let savedScore
-  let savedScore2
-  
-  const [score, setScore] = useState(0);
-  const [holdingScore, setHoldingScore] = useState(score);
-  
-  const [score2, setScore2] = useState(0);
-  const [holdingScore2, setHoldingScore2] = useState(score);
-  
+  const [count, setCount] = useState(0);
   const [wordCheck, setWordCheck] = useState()
-  
+  const [playerCount, setPlayerCount] = useState()
+  const [players, setPlayers] = useState()
+
   useEffect(() => {
-    let savedScore = localStorage.getItem('score1')
-    let savedScore2 = localStorage.getItem('score2')
-    setScore(+savedScore)
-    setScore2(+savedScore2)
+    let playerCount = localStorage.getItem('player-count')
+    let players = localStorage.getItem('players') ? JSON.parse(localStorage.getItem('players')) : ''
+    setPlayers(players)
+    setPlayerCount(+playerCount)
   }, [])
   
-  const addScore = () => {
-    localStorage.setItem("score1", (+holdingScore + +score))
-    savedScore =  localStorage.getItem('score1')
-    setScore(savedScore)
-    const scoreInput = document.getElementById("score-input")
-    scoreInput.value = ""
-  }
-  
-  const clearScore = () => {
-    localStorage.setItem("score1", 0)
-    savedScore = typeof window !== 'undefined' ? localStorage.getItem('score1') : null
-    setScore(0)
-    setHoldingScore(0)
-    const scoreInput = document.getElementById("score-input")
-    scoreInput.value = ""
-  }
-  
-  const addScore2 = () => {
-    localStorage.setItem("score2", (+holdingScore2 + +score2))
-    savedScore2 = typeof window !== 'undefined' ? localStorage.getItem('score2') : null
-    setScore2(holdingScore2)
-    const scoreInput = document.getElementById("score-input2")
-    scoreInput.value = ""
-  }
-  
-  const clearScore2 = () => {
-    localStorage.setItem("score2", 0)
-    savedScore2 = typeof window !== 'undefined' ? localStorage.getItem('score2') : null
-    setScore2(0)
-    setHoldingScore2(0)
-    const scoreInput = document.getElementById("score-input2")
-    scoreInput.value = ""
-  }
   
   const generateURL = (wordToCheck) => {
-    
-    // const API = {
-    //   CORS: 'https://cors-anywhere.herokuapp.com/',
-    //   baseURL: 'http://www.wordgamedictionary.com/api/v1/references/scrabble/',
-    //   key: '7.304453775081076e29',
-    // }
-    // //takes in a word and returns a properly formatted URL for the API
-    //   return API.CORS + API.baseURL + wordToCheck + '?key=' + API.key;
-    // }
     const API = {
-      // CORS: 'https://cors-anywhere.herokuapp.com/',
       baseURL: `https://api.dictionaryapi.dev/api/v2/entries/en/${wordToCheck}`,
-      // key: '7.304453775081076e29',
     }
-    //takes in a word and returns a properly formatted URL for the API
       return API.baseURL
     }
 
   const callAPI = async (word) =>  {
-      //generate a URL for the specified word, then call the fetch API
       const response = await fetch(generateURL(word), { method: 'get' })
-      //get the response in a string format
       const parsedResponse = await response.json();
       return parsedResponse
-      // const parsedResponse = await response.text()
-      // const toXML = window !== undefined ? (await (new window.DOMParser()).parseFromString(parsedResponse, "text/xml")) : ''
-      // return toXML
   }
 
   const generateWordResponse = () => {
-    if (wordCheck?.length) {
+    if (wordCheck?.length > 0) {
       return <>This is a scrabble word! <a target='_blank' rel='noreferrer' href={`${wordCheck[0].sourceUrls[0]}`}>Meaning</a></>
     } else if (wordCheck?.message) {
       return 'This is not a scrabble word!'
@@ -96,9 +37,107 @@ export default function Index() {
       else return ''
   }
 
+  const localSetPlayerCount = (playerCount) => {
+    localStorage.setItem("player-count", playerCount)
+    setPlayerCount(playerCount)
+  }
+
+  const playerScore = (player) => {
+    const score = localStorage.getItem(player)
+    if (score) return score
+    else return 0
+  }
+  
+  const setPlayerScore = (player) => {
+    const playerEl = document.querySelector(`.${player}`)
+    const score = playerEl.value
+    localStorage.setItem(player, (+score + +playerScore(player)))
+    playerEl.value = ''
+    setCount(count + 1)
+  }
+  
+  const clearScore = (player) => {
+    localStorage.setItem(player, 0)
+    setCount(count + 1)
+  }
+
+  const startGame = (playerNames) => {
+    let names = []
+    playerNames.forEach(name => names.push(name.value))
+    for ( let val of names) {
+      var matches = val.match(/\d+/g);
+      if (matches != null) {
+        playerNames.forEach(name => name.value = '')
+        alert('Name must not contain numbers');
+        return
+      }
+    }
+    const duplicateElements = names.filter((item, index) => names.indexOf(item) !== index)
+    if (duplicateElements.length > 0) {
+      playerNames.forEach(name => name.value = '')
+      alert('Names must be unique');
+      return
+    }
+    const storageNames = JSON.stringify(names)
+    localStorage.setItem('players', storageNames)
+    setPlayers(names)
+  }
+
+  const resetGame = () => {
+    localStorage.clear()
+    location.reload()
+  }
+
+  const renderPlayerInputs = () => {
+    const playerNameArr = []
+    for (let i = 0; i < playerCount; i++) {
+      playerNameArr.push(
+        <input 
+          type="text" 
+          className="player-name" 
+          maxLength="9"
+          placeholder="name"
+        />
+      )
+    }
+    return playerNameArr
+  }
+
+  const renderPlayerBoxes = () => {
+    const playerBoxes = []
+    for (let player of players) {
+      const tileArr =[]
+      for (let i = 0; i < player.length; i++) {
+        tileArr.push(
+          <div className="tile" data-letter={player[i]}></div>
+        )
+      }
+      playerBoxes.push(
+        <>
+        <div className="rack">
+        {tileArr}
+        </div>
+        <p className="score">SCORE: {playerScore(player)}</p>
+        <div className="score-block">
+          <input id="score-input" className={`inputs ${player}`} type="number"/>
+          <div className="add-btn">
+            <button onClick={() => setPlayerScore(player)} className="add-score">Add</button>
+          </div>
+          <div className="clear-btn">
+            <button onClick={() => clearScore(player)} className="add-score">Clear</button>
+          </div>
+        </div>
+        <hr />
+        </>
+      )
+    }
+    return playerBoxes
+  }
+
   return (
     <>
     <div style={{ lineHeight: "1.4" }}>
+      <div className="reset-game" onClick={() => resetGame()}>RESET</div>
       <h1>SCRABBLE MASTER</h1>
       <div className="word-checker">
       <input type="text" className="word-input" />
@@ -121,38 +160,37 @@ export default function Index() {
       </div>
       <hr />
       <div className="score-display">
-        <div className="rack">
-          <div className="tile" data-letter="o"></div>
-          <div className="tile" data-letter="l"></div>
-          <div className="tile" data-letter="y"></div>
-          <div className="tile" data-letter="a"></div>
-        </div>
-        <p className="score">SCORE: {score > 0 ? score : savedScore}</p>
-        <div className="score-block">
-          <input id="score-input" className="inputs" type="number" onChange={e => setHoldingScore(+e.target.value)} />
-          <div className="add-btn">
-            <button onClick={() => addScore()} className="add-score">Add</button>
+        {players ? 
+        renderPlayerBoxes()  
+        :
+        '' }
+        { playerCount && !players ?
+            <div className="name-inputs">
+              {renderPlayerInputs()}
+              <button onClick={async () => {
+                const playerNames = document.querySelectorAll('.player-name')
+                startGame(playerNames)
+              }}>
+                Start Game!
+                </button>
           </div>
-          <div className="clear-btn">
-            <button onClick={() => clearScore()} className="add-score">Clear</button>
+        : 
+          players ? '' :
+          <div className="count-box">
+            <label for="player-count">Player count:</label>
+            <select name="player-count" id="player-count">
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+            <button className="btn" onClick={() => {
+              localSetPlayerCount(document.getElementById("player-count").value)
+            }}
+            >
+              Add players
+            </button>
           </div>
-        </div>
-        <hr />
-        <div className="rack">
-          <div className="tile" data-letter="t"></div>
-          <div className="tile" data-letter="y"></div>
-        </div>
-        <p className="score">SCORE: {score2 > 0 ? score2 : savedScore2}</p> 
-        <div className="score-block">
-          <input id="score-input2" className="inputs" type="number" onChange={e => setHoldingScore2(+e.target.value)} />
-          <div className="add-btn">
-            <button onClick={() => addScore2()} className="add-score">Add</button>
-          </div>
-          <div className="clear-btn">
-            <button onClick={() => clearScore2()} className="add-score">Clear</button>
-          </div>
-        </div>
-        <hr />
+        }
       </div>
     </div>
   </>
